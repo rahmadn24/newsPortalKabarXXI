4f0ad4f1b0a0aaa27a719f16bac35b0ed1cf81e8
abitulkhah <abimeiiku@gmail.com>
2019-09-17 15:03:01 +0700

    check https

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'kabarxx1MdB';
+export class AppComponent implements OnInit {
+
+  location: Location;
+  ngOnInit() {
+
+    if (environment.production) {
+      if (location.protocol === 'http:') {
+        window.location.href = location.href.replace('http', 'https');
+      }
+    }
+
+  }
+
 }
